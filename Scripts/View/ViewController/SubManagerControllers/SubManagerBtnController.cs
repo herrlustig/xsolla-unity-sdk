d9876bc84d6f9312893724d9ab5bf93bec2a45e8
YurkinPavel <p.yurkin@xsolla.com>
2017-02-15 12:14:42 +0500

    [update] clear from UnityEditor pref. Update design fonts

--- Scripts/View/ViewController/SubManagerControllers/SubManagerBtnController.cs
+++ Scripts/View/ViewController/SubManagerControllers/SubManagerBtnController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using UnityEngine;
-using UnityEditor;
 using UnityEngine.UI;
 
 namespace Xsolla
--- Scripts/View/ViewController/SubsControllers/SubsManagerController.cs
+++ Scripts/View/ViewController/SubsControllers/SubsManagerController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using UnityEngine;
-using UnityEditor;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
